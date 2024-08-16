d5984e409830ea63d7d09da502955d72fc1a1e02
tommats00 <tbg@bren.ucsb.edu>
2024-08-16 13:27:59 -0700

    Initial commit

---
+++ R/coffee_function.R
@@ -0,0 +1,24 @@
+
+# Create a function
+
+#' Coffee Function
+#'
+#' @param brand What brand of coffee "Character"
+#' @param caffeine How many mg of caffeine is in it "Numeric"
+#' @param size What size the coffee cup is
+#'
+#' @return
+#' @export
+#'
+#' @examples coffee_function("kirkland", 100, "large")
+coffee_function <- function(brand, caffeine, size) {
+  if (brand == "kirkland" & caffeine >= 100 & size == "large") {
+    print("The best coffee")
+  } else if (brand == starbucks & caffeine <= 150 & size == "small") {
+    print("the worst coffee")
+  } else {
+    print("I dont like coffee")
+  }
+}
+
+
